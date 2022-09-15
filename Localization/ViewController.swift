4bb4f3bd977621522074c3470b2d2fe5d0f32307
codemanager255 <codemanager255@gmail.com>
2022-09-15 05:21:54 -0400

    added search feature

--- Localization/ViewController.swift
+++ Localization/ViewController.swift
@@ -14,8 +14,17 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading
-        
-       
+        let keyword = ""
+        performSegue(withIdentifier:"search", sender: keyword)
+    }
+
+
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
     }
 
 
