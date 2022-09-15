af392a7c38ef35e77b6e08c14cbf273f60128dfb
codemanager255 <codemanager255@gmail.com>
2022-09-15 05:05:35 -0400

    added localization

--- Localization/ViewController.swift
+++ Localization/ViewController.swift
@@ -9,11 +9,31 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var tableView: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        // Do any additional setup after loading
+        
+       
     }
 
 
 }
 
+extension ViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 100
+    }
+    
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier:"cell", for: indexPath)
+        
+        cell.textLabel?.text = "\(indexPath.row)"
+        return cell
+    }
+    
+    
+}
