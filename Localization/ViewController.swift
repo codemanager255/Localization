d9a7f5f87ac70accdd3026db26295c1b84d83deb
codemanager255 <codemanager255@gmail.com>
2022-09-15 05:15:54 -0400

    add loginViewController

--- Localization/ViewController.swift
+++ Localization/ViewController.swift
@@ -14,11 +14,19 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading
-        
-       
+        let name = "Adam"
+        performSegue(withIdentifier: "Login", sender: name)
     }
 
 
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        
+        segue.destination as? LoginViewController
+        
+       let name =  sender as? String
+    }
+    
 }
 
 extension ViewController: UITableViewDataSource {
